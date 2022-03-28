4aff4bac4632ca17ec4b9b0fca0ffe12b7b994ef
Programador Michel Rosales <95501615+Michprosal003@users.noreply.github.com>
2022-03-28 06:56:24 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_Triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            // Calcular Area del Triangulo
+
+            Console.Title = " Area del Triangulo";
+            Console.WriteLine("Ingresa la base del triangulo");
+            int base_t = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Ingresa la altura del triangulo");
+            int altura = int.Parse(Console.ReadLine());
+
+            double area = (base_t * altura) / 2;
+
+            Console.WriteLine("El area del triangulo es : " + area + "   metros²   ");
+
+            Console.ReadKey();
+        }
+    }
+}
